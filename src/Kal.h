a552399834fd4acc401d08ccd6ce2860026c7bad
于磊 <lei.yu1@renren-inc.com>
2014-06-22 16:26:51 +0800

    修改宏定义

--- src/Kal.h
+++ src/Kal.h
@@ -45,3 +45,6 @@ extern NSString *const KalDataSourceChangedNotification;
 #define kDarkGrayColor       RGBCOLOR(51, 51, 51)
 #define kGrayColor           RGBCOLOR(153, 153, 153)
 #define kLightGrayColor      RGBCOLOR(185, 185, 185)
+
+#define RGBCOLOR(R,G,B) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:1]
+#define RGBACOLOR(R,G,B,A) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:A]
